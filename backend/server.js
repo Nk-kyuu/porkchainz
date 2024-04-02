306041c41be0e6fddbf2c4dc5c18d035b4e1ce5e
ipercentage <chonthitta.chinawong@gmail.com>
2024-04-02 22:04:15 +0700

    edit

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,17 @@
 const express = require('express');
 require('dotenv').config();
 
-// Create an Express application
+const test = require('./test')
+const farmer = require('./farmer')
+const batch = require('./batch')
+
+const pig = require('./pigTable');
+const farmerAdd = require('./farmerAdd') //add pig**
+const farmerAddBatch = require('./farmerAddBatch') //add batch**
+const slaughtererAdd = require('./slaughtererAdd') //add product**
+const slaughtererSend = require('./slaughtererSend') //add product**
+const login = require('./login')
+
 const app = express();
 //post
 
@@ -14,6 +24,28 @@ app.get('/', (req, res) => {
 });
 
 // Start the server
+// const logger = (req, res, next) => {
+//   console.log(`${req.protocol}://${req.get('host')}${req.orinalUrl}`);
+//   next();
+// }
+
+// app.use(logger);
+
+app.use("/", test);
+
+app.use("/", farmer);
+
+app.use("/", batch);
+
+app.use("/", pig);
+
+
+app.use("/", farmerAdd);
+app.use("/", farmerAddBatch);
+app.use("/", slaughtererAdd );
+app.use("/", slaughtererSend );
+app.use("/", login);
+
 const port = process.env.PORT 
 
 app.listen(port, () => {
