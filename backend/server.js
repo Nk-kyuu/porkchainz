8649c2fc11ef442f5806830e751e4411cf9d9a7a
Marltiniye <119027588+Marltiniye@users.noreply.github.com>
2024-04-02 21:59:05 +0700

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 require('dotenv').config();
+const test = require('./test');
 const pig = require('./pigTable');
-const test = require('./test')
-const farmerAdd = require('./farmerAdd') //add pig**
-const farmerAddBatch = require('./farmerAddBatch') //add batch**
-const slaughtererAdd = require('./slaughtererAdd') //add product**
-const slaughtererSend = require('./slaughtererSend') //add product**
 const app = express();
 //post
 
@@ -21,13 +17,8 @@ app.use("/", test);
 app.use("/", pig);
 
 
-app.use("/", farmerAdd);
-app.use("/", farmerAddBatch);
-app.use("/", slaughtererAdd );
-app.use("/", slaughtererSend );
 
 const port = process.env.PORT 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
