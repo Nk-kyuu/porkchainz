b6917e2c2f1b7562aaf78e50482132e1cc40d501
IngkamonCH <namkhing1830@gmail.com>
2024-04-06 05:28:39 +0700

    k

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,21 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
-
-const { API_URL, PRIVATE_KEY } = process.env;
-
 module.exports = {
   solidity: "0.8.24",
-  defaultNetwork: "MATIC",
-   networks: {
-     hardhat: {},
-     MATIC: {
-      url: API_URL,
-      accounts: [`0x${PRIVATE_KEY}`],
+  networks: {
+    hardhat: {},
+    MATIC: {
+      url: process.env.API_URL || 'http://127.0.0.1:7545', // Use default URL if environment variable not set
+      accounts: [process.env.PRIVATE_KEY],
       gas: 3000000, // Increase the gas limit to a higher value
       gasPrice: 8000000000,
-     },
-   },
-
+    },
+  },
 };
--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,18 +1,21 @@
-// const { ethers } = require("ethers");
+const { ethers } = require("hardhat");
 
 async function main() {
-  const SupplyChain = await ethers.getContractFactory("SupplyChain");
-  const supplyChain = await SupplyChain.deploy();
-
-  console.log("SupplyChain deployed to:", supplyChain.address);
+  try {
+    const SupplyChain = await ethers.getContractFactory("SupplyChain");
+    console.log("Deploying SupplyChain contract...");
+    const supplyChain = await SupplyChain.deploy();
+    
+    console.log("SupplyChain contract deployed:", supplyChain);
+    console.log("SupplyChain contract address:", supplyChain.address);
+  } catch (error) {
+    console.error("Error deploying contract:", error);
+    process.exit(1);
+  }
 }
 
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main();
+
 
 
 // Deploy
